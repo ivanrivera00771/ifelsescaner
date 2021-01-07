9355fbc869796e152c7188f857fec57313f1c585
ivanrivera00771 <ivanrivera00771@gmail.com>
2021-01-07 13:21:01 -0600

    CONDICIONALES

---
+++ ifelsescaner.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+
+public class ifelsescaner{
+
+public static void main (String[] args) {
+       
+	int numero, dato = 5;
+
+	System.out.println("introduce el numero 5");
+
+	Scanner entrada = new Scanner(System.in);
+	numero = entrada.nextInt();
+
+	if (numero ==5){
+	      
+	System.out.println("EL NUMERO ES CORRECTO");
+	
+		
+
+	}
+
+	else {
+
+	System.out.println("EL NUMERO ES INCORRECTO");
+
+
+
+
+
+	}	
+
+
+
+
+
+}
+
+
+
+}
